443ed1d6d88f4ebb77771a35374c0a73106f4588
j-brooke <6962885+j-brooke@users.noreply.github.com>
2022-10-18 15:11:36 -0400

    Added East Asian character tests

--- test/CommentFormatting.test.ts
+++ test/CommentFormatting.test.ts
@@ -222,7 +222,7 @@ describe("Comment formatting tests", () => {
 /**
  * Tests that the first occurence of the substring occurs at the same index in each line, if the line contains it.
  */
-export function DoInstancesLineUp(lines: string[], substring: string): boolean {
+function DoInstancesLineUp(lines: string[], substring: string): boolean {
     const indices = lines.map(str => str.indexOf(substring))
         .filter(num => num >= 0);
     return indices.length==0 || indices.every(num => num == indices[0]);
---
+++ test/EastAsianWideCharacters.test.ts
@@ -0,0 +1,50 @@
+﻿// @ts-ignore
+import * as eaw from 'eastasianwidth';
+import {Formatter} from "../src/Formatter";
+
+describe("East Asian wide character tests", function () {
+    test("Pads wide chars correctly", () => {
+        const inputLines = [
+            "[",
+            "    {'Name': '李小龍', 'Job': 'Actor', 'Born': 1940},",
+            "    {'Name': 'Mark Twain', 'Job': 'Writer', 'Born': 1835},",
+            "    {'Name': '孫子', 'Job': 'General', 'Born': -544}",
+            "]"
+        ];
+        const input = inputLines.join("\n").replace(/'/g, '"');
+        const formatter = new Formatter();
+
+        let output = formatter.Reformat(input, 0);
+        let outputLines = output.trimEnd().split('\n');
+
+        // With the default StringLengthFunc, all characters are treated as having the same width as space, so
+        // String.IndexOf should give the same number for each row.
+        expect(DoInstancesLineUp(outputLines, "Job")).toBeTruthy();
+        expect(DoInstancesLineUp(outputLines, "Born")).toBeTruthy();
+
+        formatter.StringLengthFunc = WideCharStringLength;
+        output = formatter.Reformat(input, 0);
+        outputLines = output.trimEnd().split('\n');
+
+        // In using the WideCharStringLength function, the Asian characters are each treated as 2 spaces wide.
+        // Whether these line up visually for you depends on your font and the rendering policies of your app.
+        // (It looks right on a Mac terminal.)
+        expect(outputLines[1].indexOf("Job")).toBe(25);
+        expect(outputLines[2].indexOf("Job")).toBe(28);
+        expect(outputLines[3].indexOf("Job")).toBe(26);
+    });
+});
+
+
+function WideCharStringLength(str: string): number {
+    return eaw.length(str);
+}
+
+/**
+ * Tests that the first occurence of the substring occurs at the same index in each line, if the line contains it.
+ */
+function DoInstancesLineUp(lines: string[], substring: string): boolean {
+    const indices = lines.map(str => str.indexOf(substring))
+        .filter(num => num >= 0);
+    return indices.length==0 || indices.every(num => num == indices[0]);
+}
