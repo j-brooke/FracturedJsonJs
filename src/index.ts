2209d8c8f9264c2d0072f00e23d4448a37872a81
j-brooke <6962885+j-brooke@users.noreply.github.com>
2022-10-20 10:13:07 -0400

    Added index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+﻿// Exporting just the basics here.
+export {FracturedJsonOptions} from './FracturedJsonOptions';
+export {CommentPolicy} from './CommentPolicy';
+export {EolStyle} from './EolStyle';
+
+export {FracturedJsonError} from './FracturedJsonError';
+
+export {Formatter} from './Formatter';
