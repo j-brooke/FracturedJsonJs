94b64ef158a5317bb3f678977a86122b9771543b
Jesse Brooke <6962885+j-brooke@users.noreply.github.com>
2024-03-30 00:03:13 -0400

    Enabled more TS safety options

--- src/FracturedJsonError.ts
+++ src/FracturedJsonError.ts
@@ -1,7 +1,7 @@
 ﻿import {InputPosition} from "./InputPosition";
 
 export class FracturedJsonError extends Error {
-    InputPosition?: InputPosition;
+    InputPosition: InputPosition | undefined;
     constructor(message?: string, pos?: InputPosition) {
         const msgWithPos = (pos)? `${message} at idx=${pos.Index}, row=${pos.Row}, col=${pos.Column}`
             : message;
