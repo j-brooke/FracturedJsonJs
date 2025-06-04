4c73f86494c3ced18b7c84cfba131989174430c2
Jesse Brooke <6962885+j-brooke@users.noreply.github.com>
2025-06-04 08:49:05 -0400

    Added TableCommaPlacement to exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {FracturedJsonOptions} from './FracturedJsonOptions';
 export {CommentPolicy} from './CommentPolicy';
 export {EolStyle} from './EolStyle';
 export {NumberListAlignment} from './NumberListAlignment';
+export {TableCommaPlacement} from './TableCommaPlacement';
 
 export {FracturedJsonError} from './FracturedJsonError';
 
